759b392c5eceb77ff60f43dfecc8ff76080e368b
greg <GregLawson123@gmail.com>
2022-01-27 05:04:51 -0800

    tests pass

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter
+using TableDirectories
+
+makedocs(
+    sitename = "TableDirectories",
+    format = Documenter.HTML(),
+    modules = [TableDirectories]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
---
+++ src/lib.jl
@@ -0,0 +1,1 @@
+TableDirectories.jl
